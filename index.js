ebe8a95c4b77f39e99c9f615ec223ae08c447941
mati gonzalez <matigonzalez@provart.com.ar>
2025-03-08 00:06:22 -0300

    updated ccors

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const server = app.listen(config.PORT || 3000,()=>{
 });
 app.use(json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+// O si solo quieres permitir localhost y tu dominio de producción:
+app.use(cors({
+    origin: ["http://localhost:5173/", "https://tu-app-en-produccion.com/","https://localhost:5173/","http://localhost:5173","https://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use('/api',controller);
 
 module.exports = {app, server};
